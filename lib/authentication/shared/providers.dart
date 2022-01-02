3cb923b2a1d5420ff70a2c767ccf6e2c64c816b9
sameer <i.amsameer2061@gmail.com>
2022-01-02 18:02:38 +0545

    Auth dependency injection

---
+++ lib/authentication/shared/providers.dart
@@ -0,0 +1,36 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:gooddhan/authentication/application/auth_notifier.dart';
+import 'package:gooddhan/authentication/infrastructure/Credential_storage/credential_storage.dart';
+import 'package:gooddhan/authentication/infrastructure/Credential_storage/secure_credential_storage.dart';
+import 'package:gooddhan/authentication/infrastructure/authenticator.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final flutterSecureStorageProvider = Provider<FlutterSecureStorage>(
+  (ref) => const FlutterSecureStorage(),
+);
+
+final credentialStorageProvider = Provider<CredentialStorage>((ref) {
+  return SecureCredentialStorage(ref.read(flutterSecureStorageProvider));
+});
+
+final googleSignInProvider = Provider<GoogleSignIn>((ref) {
+  return GoogleSignIn();
+});
+final dioProvider = Provider<Dio>((ref) {
+  return Dio();
+});
+final authenticatorProvidr = Provider<Authenticator>(
+  (ref) {
+    return Authenticator(
+      credentialStorage: ref.read(credentialStorageProvider),
+      googleSignIn: ref.read(googleSignInProvider),
+      dio: ref.read(dioProvider),
+    );
+  },
+);
+
+final authNotifierProvider = StateNotifierProvider<AuthNotifier, AuthState>(
+  (ref) => AuthNotifier(ref.read(authenticatorProvidr)),
+);
