3f8260888831aeaa6d4dc421d32dd448113748a9
sameer <i.amsameer2061@gmail.com>
2022-01-02 17:59:48 +0545

    custom widget: custom value tile

---
+++ lib/core/shared/widgets/custom_value_tile.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:gooddhan/core/infrastructure/text_theme_extension.dart';
+
+class CustomValueTile extends StatelessWidget {
+  final String title;
+  final String? value;
+  final Widget? trailing;
+  final void Function()? onTap;
+
+  const CustomValueTile({
+    Key? key,
+    required this.title,
+    this.trailing,
+    this.onTap,
+    this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        padding: const EdgeInsets.all(5),
+        child: Row(
+          children: [
+            Text(
+              title,
+              style: context.headline2?.copyWith(fontWeight: FontWeight.w500),
+            ),
+            const Spacer(),
+            _buildTrailing(context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildTrailing(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
+
+    if (trailing != null) {
+      return trailing;
+    }
+    return Row(
+      children: [
+        Text(value ?? ""),
+        const SizedBox(width: 10),
+        Icon(
+          Icons.arrow_forward_ios,
+          color: primaryColor,
+          size: 17,
+        )
+      ],
+    );
+  }
+}
