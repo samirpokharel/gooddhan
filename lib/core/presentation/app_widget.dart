c04f26e66890b54c2b432615b54f49b42d6643a5
sameer <i.amsameer2061@gmail.com>
2022-01-02 18:03:04 +0545

    Auth widget initlizer and auth status check

--- lib/core/presentation/app_widget.dart
+++ lib/core/presentation/app_widget.dart
@@ -1,7 +1,16 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gooddhan/authentication/application/auth_notifier.dart';
+import 'package:gooddhan/authentication/shared/providers.dart';
 import 'package:gooddhan/core/presentation/routes/app_router.gr.dart';
 import 'package:gooddhan/core/presentation/themes/app_theme.dart';
+
+final initializationProvider = FutureProvider<Unit>((ref) async {
+  final authNotifier = ref.read(authNotifierProvider.notifier);
+  await authNotifier.checkAndUpdateAuthStatus();
+  return unit;
+});
 
 class AppWidget extends ConsumerWidget {
   final appRouter = AppRouter();
@@ -9,6 +18,22 @@ class AppWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
+    ref.listen(initializationProvider, (_, __) {});
+    ref.listen<AuthState>(
+      authNotifierProvider,
+      (previous, next) {
+        next.maybeMap(
+          orElse: () {},
+          authenticated: (_) {},
+          unauthenticated: (_) {
+            appRouter.pushAndPopUntil(
+              const AuthSwitcherRoute(),
+              predicate: (route) => false,
+            );
+          },
+        );
+      },
+    );
     return MaterialApp.router(
       theme: AppTheme.light(),
       routeInformationParser: appRouter.defaultRouteParser(),
