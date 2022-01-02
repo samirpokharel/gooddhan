3e558f4f9dbc4538fa752231466f221e569e7af3
sameer <i.amsameer2061@gmail.com>
2022-01-02 13:57:13 +0545

    implement auth notifier create account login account and signout

--- lib/authentication/application/auth_notifier.dart
+++ lib/authentication/application/auth_notifier.dart
@@ -14,6 +14,8 @@ class AuthState with _$AuthState {
   const factory AuthState.failure() = _AuthFailed;
 }
 
+typedef AuthCallback = Future<String?> Function(String? idToken);
+
 class AuthNotifier extends StateNotifier<AuthState> {
   final Authenticator _authenticator;
   AuthNotifier(this._authenticator) : super(const AuthState.initial());
@@ -22,5 +24,40 @@ class AuthNotifier extends StateNotifier<AuthState> {
     state = (await _authenticator.isSigned)
         ? const AuthState.authenticated()
         : const AuthState.unauthenticated();
+  }
+
+  Future<void> login(AuthCallback authCallback) async {
+    final idToken = await authCallback(await _authenticator.getGoogleIdToken());
+    if (idToken != null) {
+      _authenticator.loginAccount(token: idToken);
+    }
+  }
+
+  Future<void> createGoogleAcountCred(AuthCallback authCallback) async {
+    await authCallback(await _authenticator.getGoogleIdToken());
+  }
+
+  Future<void> createAccount(
+    String token, {
+    required String currency,
+    required String monthlyIncome,
+  }) async {
+    final failureOrSuccess = await _authenticator.createAccount(
+      currency: currency,
+      monthlyIncome: monthlyIncome,
+      token: token,
+    );
+    state = failureOrSuccess.fold(
+      (l) => const AuthState.failure(),
+      (r) => const AuthState.authenticated(),
+    );
+  }
+
+  Future<void> signOut() async {
+    final failureOrSuccess = await _authenticator.signOut();
+    state = failureOrSuccess.fold(
+      (l) => const AuthState.failure(),
+      (r) => const AuthState.unauthenticated(),
+    );
   }
 }
