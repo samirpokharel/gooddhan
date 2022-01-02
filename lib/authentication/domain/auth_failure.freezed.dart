a4b04fe21050bb3a9e3392893b25eea1ca56c52b
sameer <i.amsameer2061@gmail.com>
2022-01-02 12:29:31 +0545

    Auth failure and vscode config

---
+++ lib/authentication/domain/auth_failure.freezed.dart
@@ -0,0 +1,295 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'auth_failure.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$AuthFailureTearOff {
+  const _$AuthFailureTearOff();
+
+  _Searver server() {
+    return const _Searver();
+  }
+
+  _Storage storage() {
+    return const _Storage();
+  }
+}
+
+/// @nodoc
+const $AuthFailure = _$AuthFailureTearOff();
+
+/// @nodoc
+mixin _$AuthFailure {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() server,
+    required TResult Function() storage,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? server,
+    TResult Function()? storage,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? server,
+    TResult Function()? storage,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Searver value) server,
+    required TResult Function(_Storage value) storage,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_Searver value)? server,
+    TResult Function(_Storage value)? storage,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Searver value)? server,
+    TResult Function(_Storage value)? storage,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AuthFailureCopyWith<$Res> {
+  factory $AuthFailureCopyWith(
+          AuthFailure value, $Res Function(AuthFailure) then) =
+      _$AuthFailureCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$AuthFailureCopyWithImpl<$Res> implements $AuthFailureCopyWith<$Res> {
+  _$AuthFailureCopyWithImpl(this._value, this._then);
+
+  final AuthFailure _value;
+  // ignore: unused_field
+  final $Res Function(AuthFailure) _then;
+}
+
+/// @nodoc
+abstract class _$SearverCopyWith<$Res> {
+  factory _$SearverCopyWith(_Searver value, $Res Function(_Searver) then) =
+      __$SearverCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$SearverCopyWithImpl<$Res> extends _$AuthFailureCopyWithImpl<$Res>
+    implements _$SearverCopyWith<$Res> {
+  __$SearverCopyWithImpl(_Searver _value, $Res Function(_Searver) _then)
+      : super(_value, (v) => _then(v as _Searver));
+
+  @override
+  _Searver get _value => super._value as _Searver;
+}
+
+/// @nodoc
+
+class _$_Searver extends _Searver {
+  const _$_Searver() : super._();
+
+  @override
+  String toString() {
+    return 'AuthFailure.server()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _Searver);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() server,
+    required TResult Function() storage,
+  }) {
+    return server();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? server,
+    TResult Function()? storage,
+  }) {
+    return server?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? server,
+    TResult Function()? storage,
+    required TResult orElse(),
+  }) {
+    if (server != null) {
+      return server();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Searver value) server,
+    required TResult Function(_Storage value) storage,
+  }) {
+    return server(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_Searver value)? server,
+    TResult Function(_Storage value)? storage,
+  }) {
+    return server?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Searver value)? server,
+    TResult Function(_Storage value)? storage,
+    required TResult orElse(),
+  }) {
+    if (server != null) {
+      return server(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Searver extends AuthFailure {
+  const factory _Searver() = _$_Searver;
+  const _Searver._() : super._();
+}
+
+/// @nodoc
+abstract class _$StorageCopyWith<$Res> {
+  factory _$StorageCopyWith(_Storage value, $Res Function(_Storage) then) =
+      __$StorageCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$StorageCopyWithImpl<$Res> extends _$AuthFailureCopyWithImpl<$Res>
+    implements _$StorageCopyWith<$Res> {
+  __$StorageCopyWithImpl(_Storage _value, $Res Function(_Storage) _then)
+      : super(_value, (v) => _then(v as _Storage));
+
+  @override
+  _Storage get _value => super._value as _Storage;
+}
+
+/// @nodoc
+
+class _$_Storage extends _Storage {
+  const _$_Storage() : super._();
+
+  @override
+  String toString() {
+    return 'AuthFailure.storage()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _Storage);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() server,
+    required TResult Function() storage,
+  }) {
+    return storage();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? server,
+    TResult Function()? storage,
+  }) {
+    return storage?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? server,
+    TResult Function()? storage,
+    required TResult orElse(),
+  }) {
+    if (storage != null) {
+      return storage();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Searver value) server,
+    required TResult Function(_Storage value) storage,
+  }) {
+    return storage(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_Searver value)? server,
+    TResult Function(_Storage value)? storage,
+  }) {
+    return storage?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Searver value)? server,
+    TResult Function(_Storage value)? storage,
+    required TResult orElse(),
+  }) {
+    if (storage != null) {
+      return storage(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Storage extends AuthFailure {
+  const factory _Storage() = _$_Storage;
+  const _Storage._() : super._();
+}
