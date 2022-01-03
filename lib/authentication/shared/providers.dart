b186bafa760c97406deb06cf5210a145c858ba31
sameer <i.amsameer2061@gmail.com>
2022-01-03 18:30:06 +0545

    user local storage provider

--- lib/authentication/shared/providers.dart
+++ lib/authentication/shared/providers.dart
@@ -4,12 +4,18 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:gooddhan/authentication/application/auth_notifier.dart';
 import 'package:gooddhan/authentication/infrastructure/Credential_storage/credential_storage.dart';
 import 'package:gooddhan/authentication/infrastructure/Credential_storage/secure_credential_storage.dart';
+import 'package:gooddhan/authentication/infrastructure/User_local_storage.dart';
 import 'package:gooddhan/authentication/infrastructure/authenticator.dart';
+import 'package:gooddhan/core/shared/providers.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 final flutterSecureStorageProvider = Provider<FlutterSecureStorage>(
   (ref) => const FlutterSecureStorage(),
 );
+
+final userlocalStorageProvider = Provider<UserLocalStorage>((ref) {
+  return UserLocalStorage(sembastDatabase: ref.read(sembastProvider));
+});
 
 final credentialStorageProvider = Provider<CredentialStorage>((ref) {
   return SecureCredentialStorage(ref.read(flutterSecureStorageProvider));
@@ -18,10 +24,7 @@ final credentialStorageProvider = Provider<CredentialStorage>((ref) {
 final googleSignInProvider = Provider<GoogleSignIn>((ref) {
   return GoogleSignIn(
     signInOption: SignInOption.standard,
-    scopes: [
-      'email',
-      // 'https://www.googleapis.com/auth/contacts.readonly',
-    ],
+    scopes: ['email'],
   );
 });
 final dioProvider = Provider<Dio>((ref) {
@@ -30,6 +33,7 @@ final dioProvider = Provider<Dio>((ref) {
 final authenticatorProvidr = Provider<Authenticator>(
   (ref) {
     return Authenticator(
+      userLocalStorage: ref.read(userlocalStorageProvider),
       credentialStorage: ref.read(credentialStorageProvider),
       googleSignIn: ref.read(googleSignInProvider),
       dio: ref.read(dioProvider),
