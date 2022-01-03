bf045e903bcaeb6dce2643ceb2d7e168cd91df46
sameer <i.amsameer2061@gmail.com>
2022-01-03 20:51:51 +0545

    button nav bar notifier

---
+++ lib/dashboard/navigation/presentation/application/bottom_nav_bar_notifier.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum BottomNavItem { home, expenses, report, profile }
+
+final bottomNavProvider =
+    StateNotifierProvider.autoDispose<NavigationController, BottomNavItem>(
+        (ref) => NavigationController());
+
+class NavigationController extends StateNotifier<BottomNavItem> {
+  NavigationController() : super(BottomNavItem.home);
+
+  void updateSelectedItem(BottomNavItem item) {
+    if (item != state) {
+      state = item;
+    }
+  }
+}
