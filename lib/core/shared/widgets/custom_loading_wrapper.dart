98ad9e5c507ef50f8990b0f20bf2e96eb614d7a0
sameer <i.amsameer2061@gmail.com>
2022-01-03 16:59:15 +0545

    Create custom loading wrapper

---
+++ lib/core/shared/widgets/custom_loading_wrapper.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomLoadingWraper extends StatelessWidget {
+  final Widget child;
+  final bool isLoading;
+  const CustomLoadingWraper({
+    Key? key,
+    required this.child,
+    required this.isLoading,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        child,
+        if (isLoading)
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white.withOpacity(.4),
+            child: Column(
+              children: const [LinearProgressIndicator()],
+            ),
+          )
+      ],
+    );
+  }
+}
