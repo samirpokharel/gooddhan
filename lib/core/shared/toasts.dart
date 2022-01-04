cd4db528782adf0ae9c6291a4c8ff47f29c92741
sameer <i.amsameer2061@gmail.com>
2022-01-04 20:09:44 +0545

    added toast flavour

--- lib/core/shared/toasts.dart
+++ lib/core/shared/toasts.dart
@@ -1,10 +1,25 @@
 import 'package:flash/flash.dart';
 import 'package:flutter/material.dart';
 
-void showErrorToast(
+enum ToastFlavouer { error, warn, success }
+
+_colorBaseOnFlavour(ToastFlavouer _) {
+  switch (_) {
+    case ToastFlavouer.success:
+      return [Colors.green, Icons.check];
+    case ToastFlavouer.warn:
+      return [Colors.amber, Icons.warning];
+    default:
+      return [Colors.red, Icons.error];
+  }
+}
+
+void showFlashToast(
   BuildContext context, {
   String? message = "Something went wrong",
   Duration? dismissDuration,
+  ToastFlavouer flavouer = ToastFlavouer.error,
+  bool barrierDismissible = false,
 }) {
   if (message!.startsWith("Dublicate field")) {
     message = "This Email is already used";
@@ -17,15 +32,29 @@ void showErrorToast(
           (value) => _.dismiss(),
         );
       }
+      final color = _colorBaseOnFlavour(flavouer)[0] as MaterialColor;
+      final icon = _colorBaseOnFlavour(flavouer)[1] as IconData;
+
       return Flash.bar(
         controller: _,
-        backgroundColor: Colors.red[100],
-        margin: const EdgeInsets.only(bottom: 30),
+        backgroundColor: color[50],
+        margin: const EdgeInsets.only(
+          bottom: 30,
+          left: 10,
+          right: 10,
+        ),
         behavior: FlashBehavior.floating,
+        barrierDismissible: barrierDismissible,
         borderRadius: BorderRadius.circular(20),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Text(message!, style: const TextStyle(color: Colors.red)),
+          child: Wrap(
+            children: [
+              Icon(icon, color: color),
+              const SizedBox(width: 3),
+              Text(message!, style: TextStyle(color: color)),
+            ],
+          ),
         ),
       );
     },
