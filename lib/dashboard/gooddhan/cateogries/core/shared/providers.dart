56c4a4da03fc1fe8893789994fdd512689cba722
sameer <i.amsameer2061@gmail.com>
2022-01-04 14:43:47 +0545

    category feature dependency injection providers

---
+++ lib/dashboard/gooddhan/cateogries/core/shared/providers.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gooddhan/core/shared/providers.dart';
+import 'package:gooddhan/dashboard/gooddhan/cateogries/core/application/paginated_category_notifier.dart';
+import 'package:gooddhan/dashboard/gooddhan/cateogries/list_categories/application/list_categories_notifier.dart';
+import 'package:gooddhan/dashboard/gooddhan/cateogries/list_categories/infrastrucuture/list_categories_local_service.dart';
+import 'package:gooddhan/dashboard/gooddhan/cateogries/list_categories/infrastrucuture/list_categories_remote_service.dart';
+import 'package:gooddhan/dashboard/gooddhan/cateogries/list_categories/infrastrucuture/list_categories_repository.dart';
+import 'package:gooddhan/dashboard/gooddhan/core/infrastructure/gooddhan_header_cache.dart';
+
+final githuHeaderCacheProvider = Provider<GooddhanHeaderCache>((ref) {
+  return GooddhanHeaderCache(ref.watch(sembastProvider));
+});
+
+final listCategoriesRemoteServiceProvider =
+    Provider<ListCategoryRemoteService>((ref) {
+  return ListCategoryRemoteService(
+    ref.watch(dioProvider),
+    ref.watch(githuHeaderCacheProvider),
+  );
+});
+
+final listCategoryLocalServiceProvider =
+    Provider<ListCategoriesLocalService>((ref) {
+  return ListCategoriesLocalService(ref.watch(sembastProvider));
+});
+
+final listCategoriesRepositoryProvider =
+    Provider<ListCategoriesRepository>((ref) {
+  return ListCategoriesRepository(
+    ref.watch(listCategoryLocalServiceProvider),
+    ref.watch(listCategoriesRemoteServiceProvider),
+  );
+});
+
+final listCategoryNotifierProvider =
+    StateNotifierProvider<ListCategoryNotifer, PaginatedCategoryState>((ref) {
+  return ListCategoryNotifer(ref.watch(listCategoriesRepositoryProvider));
+});
