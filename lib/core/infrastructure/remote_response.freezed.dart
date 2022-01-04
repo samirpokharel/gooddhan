a06183c477153c9bfe564f8e2258dae153518d52
sameer <i.amsameer2061@gmail.com>
2022-01-04 14:26:54 +0545

    Remote response generic class

---
+++ lib/core/infrastructure/remote_response.freezed.dart
@@ -0,0 +1,511 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'remote_response.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$RemoteResponseTearOff {
+  const _$RemoteResponseTearOff();
+
+  _NoConnection<T> noConnection<T>() {
+    return _NoConnection<T>();
+  }
+
+  _NotModified<T> notModified<T>({required int totalPage}) {
+    return _NotModified<T>(
+      totalPage: totalPage,
+    );
+  }
+
+  _WithNewData<T> withNewData<T>(T data, {required int totalpage}) {
+    return _WithNewData<T>(
+      data,
+      totalpage: totalpage,
+    );
+  }
+}
+
+/// @nodoc
+const $RemoteResponse = _$RemoteResponseTearOff();
+
+/// @nodoc
+mixin _$RemoteResponse<T> {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() noConnection,
+    required TResult Function(int totalPage) notModified,
+    required TResult Function(T data, int totalpage) withNewData,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? noConnection,
+    TResult Function(int totalPage)? notModified,
+    TResult Function(T data, int totalpage)? withNewData,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? noConnection,
+    TResult Function(int totalPage)? notModified,
+    TResult Function(T data, int totalpage)? withNewData,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_NoConnection<T> value) noConnection,
+    required TResult Function(_NotModified<T> value) notModified,
+    required TResult Function(_WithNewData<T> value) withNewData,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_NoConnection<T> value)? noConnection,
+    TResult Function(_NotModified<T> value)? notModified,
+    TResult Function(_WithNewData<T> value)? withNewData,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_NoConnection<T> value)? noConnection,
+    TResult Function(_NotModified<T> value)? notModified,
+    TResult Function(_WithNewData<T> value)? withNewData,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RemoteResponseCopyWith<T, $Res> {
+  factory $RemoteResponseCopyWith(
+          RemoteResponse<T> value, $Res Function(RemoteResponse<T>) then) =
+      _$RemoteResponseCopyWithImpl<T, $Res>;
+}
+
+/// @nodoc
+class _$RemoteResponseCopyWithImpl<T, $Res>
+    implements $RemoteResponseCopyWith<T, $Res> {
+  _$RemoteResponseCopyWithImpl(this._value, this._then);
+
+  final RemoteResponse<T> _value;
+  // ignore: unused_field
+  final $Res Function(RemoteResponse<T>) _then;
+}
+
+/// @nodoc
+abstract class _$NoConnectionCopyWith<T, $Res> {
+  factory _$NoConnectionCopyWith(
+          _NoConnection<T> value, $Res Function(_NoConnection<T>) then) =
+      __$NoConnectionCopyWithImpl<T, $Res>;
+}
+
+/// @nodoc
+class __$NoConnectionCopyWithImpl<T, $Res>
+    extends _$RemoteResponseCopyWithImpl<T, $Res>
+    implements _$NoConnectionCopyWith<T, $Res> {
+  __$NoConnectionCopyWithImpl(
+      _NoConnection<T> _value, $Res Function(_NoConnection<T>) _then)
+      : super(_value, (v) => _then(v as _NoConnection<T>));
+
+  @override
+  _NoConnection<T> get _value => super._value as _NoConnection<T>;
+}
+
+/// @nodoc
+
+class _$_NoConnection<T> extends _NoConnection<T> {
+  const _$_NoConnection() : super._();
+
+  @override
+  String toString() {
+    return 'RemoteResponse<$T>.noConnection()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _NoConnection<T>);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() noConnection,
+    required TResult Function(int totalPage) notModified,
+    required TResult Function(T data, int totalpage) withNewData,
+  }) {
+    return noConnection();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? noConnection,
+    TResult Function(int totalPage)? notModified,
+    TResult Function(T data, int totalpage)? withNewData,
+  }) {
+    return noConnection?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? noConnection,
+    TResult Function(int totalPage)? notModified,
+    TResult Function(T data, int totalpage)? withNewData,
+    required TResult orElse(),
+  }) {
+    if (noConnection != null) {
+      return noConnection();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_NoConnection<T> value) noConnection,
+    required TResult Function(_NotModified<T> value) notModified,
+    required TResult Function(_WithNewData<T> value) withNewData,
+  }) {
+    return noConnection(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_NoConnection<T> value)? noConnection,
+    TResult Function(_NotModified<T> value)? notModified,
+    TResult Function(_WithNewData<T> value)? withNewData,
+  }) {
+    return noConnection?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_NoConnection<T> value)? noConnection,
+    TResult Function(_NotModified<T> value)? notModified,
+    TResult Function(_WithNewData<T> value)? withNewData,
+    required TResult orElse(),
+  }) {
+    if (noConnection != null) {
+      return noConnection(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _NoConnection<T> extends RemoteResponse<T> {
+  const factory _NoConnection() = _$_NoConnection<T>;
+  const _NoConnection._() : super._();
+}
+
+/// @nodoc
+abstract class _$NotModifiedCopyWith<T, $Res> {
+  factory _$NotModifiedCopyWith(
+          _NotModified<T> value, $Res Function(_NotModified<T>) then) =
+      __$NotModifiedCopyWithImpl<T, $Res>;
+  $Res call({int totalPage});
+}
+
+/// @nodoc
+class __$NotModifiedCopyWithImpl<T, $Res>
+    extends _$RemoteResponseCopyWithImpl<T, $Res>
+    implements _$NotModifiedCopyWith<T, $Res> {
+  __$NotModifiedCopyWithImpl(
+      _NotModified<T> _value, $Res Function(_NotModified<T>) _then)
+      : super(_value, (v) => _then(v as _NotModified<T>));
+
+  @override
+  _NotModified<T> get _value => super._value as _NotModified<T>;
+
+  @override
+  $Res call({
+    Object? totalPage = freezed,
+  }) {
+    return _then(_NotModified<T>(
+      totalPage: totalPage == freezed
+          ? _value.totalPage
+          : totalPage // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_NotModified<T> extends _NotModified<T> {
+  const _$_NotModified({required this.totalPage}) : super._();
+
+  @override
+  final int totalPage;
+
+  @override
+  String toString() {
+    return 'RemoteResponse<$T>.notModified(totalPage: $totalPage)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _NotModified<T> &&
+            const DeepCollectionEquality().equals(other.totalPage, totalPage));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(totalPage));
+
+  @JsonKey(ignore: true)
+  @override
+  _$NotModifiedCopyWith<T, _NotModified<T>> get copyWith =>
+      __$NotModifiedCopyWithImpl<T, _NotModified<T>>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() noConnection,
+    required TResult Function(int totalPage) notModified,
+    required TResult Function(T data, int totalpage) withNewData,
+  }) {
+    return notModified(totalPage);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? noConnection,
+    TResult Function(int totalPage)? notModified,
+    TResult Function(T data, int totalpage)? withNewData,
+  }) {
+    return notModified?.call(totalPage);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? noConnection,
+    TResult Function(int totalPage)? notModified,
+    TResult Function(T data, int totalpage)? withNewData,
+    required TResult orElse(),
+  }) {
+    if (notModified != null) {
+      return notModified(totalPage);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_NoConnection<T> value) noConnection,
+    required TResult Function(_NotModified<T> value) notModified,
+    required TResult Function(_WithNewData<T> value) withNewData,
+  }) {
+    return notModified(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_NoConnection<T> value)? noConnection,
+    TResult Function(_NotModified<T> value)? notModified,
+    TResult Function(_WithNewData<T> value)? withNewData,
+  }) {
+    return notModified?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_NoConnection<T> value)? noConnection,
+    TResult Function(_NotModified<T> value)? notModified,
+    TResult Function(_WithNewData<T> value)? withNewData,
+    required TResult orElse(),
+  }) {
+    if (notModified != null) {
+      return notModified(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _NotModified<T> extends RemoteResponse<T> {
+  const factory _NotModified({required int totalPage}) = _$_NotModified<T>;
+  const _NotModified._() : super._();
+
+  int get totalPage;
+  @JsonKey(ignore: true)
+  _$NotModifiedCopyWith<T, _NotModified<T>> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class _$WithNewDataCopyWith<T, $Res> {
+  factory _$WithNewDataCopyWith(
+          _WithNewData<T> value, $Res Function(_WithNewData<T>) then) =
+      __$WithNewDataCopyWithImpl<T, $Res>;
+  $Res call({T data, int totalpage});
+}
+
+/// @nodoc
+class __$WithNewDataCopyWithImpl<T, $Res>
+    extends _$RemoteResponseCopyWithImpl<T, $Res>
+    implements _$WithNewDataCopyWith<T, $Res> {
+  __$WithNewDataCopyWithImpl(
+      _WithNewData<T> _value, $Res Function(_WithNewData<T>) _then)
+      : super(_value, (v) => _then(v as _WithNewData<T>));
+
+  @override
+  _WithNewData<T> get _value => super._value as _WithNewData<T>;
+
+  @override
+  $Res call({
+    Object? data = freezed,
+    Object? totalpage = freezed,
+  }) {
+    return _then(_WithNewData<T>(
+      data == freezed
+          ? _value.data
+          : data // ignore: cast_nullable_to_non_nullable
+              as T,
+      totalpage: totalpage == freezed
+          ? _value.totalpage
+          : totalpage // ignore: cast_nullable_to_non_nullable
+              as int,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_WithNewData<T> extends _WithNewData<T> {
+  const _$_WithNewData(this.data, {required this.totalpage}) : super._();
+
+  @override
+  final T data;
+  @override
+  final int totalpage;
+
+  @override
+  String toString() {
+    return 'RemoteResponse<$T>.withNewData(data: $data, totalpage: $totalpage)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _WithNewData<T> &&
+            const DeepCollectionEquality().equals(other.data, data) &&
+            const DeepCollectionEquality().equals(other.totalpage, totalpage));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(data),
+      const DeepCollectionEquality().hash(totalpage));
+
+  @JsonKey(ignore: true)
+  @override
+  _$WithNewDataCopyWith<T, _WithNewData<T>> get copyWith =>
+      __$WithNewDataCopyWithImpl<T, _WithNewData<T>>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() noConnection,
+    required TResult Function(int totalPage) notModified,
+    required TResult Function(T data, int totalpage) withNewData,
+  }) {
+    return withNewData(data, totalpage);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? noConnection,
+    TResult Function(int totalPage)? notModified,
+    TResult Function(T data, int totalpage)? withNewData,
+  }) {
+    return withNewData?.call(data, totalpage);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? noConnection,
+    TResult Function(int totalPage)? notModified,
+    TResult Function(T data, int totalpage)? withNewData,
+    required TResult orElse(),
+  }) {
+    if (withNewData != null) {
+      return withNewData(data, totalpage);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_NoConnection<T> value) noConnection,
+    required TResult Function(_NotModified<T> value) notModified,
+    required TResult Function(_WithNewData<T> value) withNewData,
+  }) {
+    return withNewData(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(_NoConnection<T> value)? noConnection,
+    TResult Function(_NotModified<T> value)? notModified,
+    TResult Function(_WithNewData<T> value)? withNewData,
+  }) {
+    return withNewData?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_NoConnection<T> value)? noConnection,
+    TResult Function(_NotModified<T> value)? notModified,
+    TResult Function(_WithNewData<T> value)? withNewData,
+    required TResult orElse(),
+  }) {
+    if (withNewData != null) {
+      return withNewData(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _WithNewData<T> extends RemoteResponse<T> {
+  const factory _WithNewData(T data, {required int totalpage}) =
+      _$_WithNewData<T>;
+  const _WithNewData._() : super._();
+
+  T get data;
+  int get totalpage;
+  @JsonKey(ignore: true)
+  _$WithNewDataCopyWith<T, _WithNewData<T>> get copyWith =>
+      throw _privateConstructorUsedError;
+}
