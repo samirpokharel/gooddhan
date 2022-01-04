6826f40bf2a6bddce331ee3cda3182cbcc0a2d9f
sameer <i.amsameer2061@gmail.com>
2022-01-04 20:12:25 +0545

    connectivity provider

--- lib/core/shared/providers.dart
+++ lib/core/shared/providers.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gooddhan/core/infrastructure/sembast_database.dart';
@@ -5,3 +6,6 @@ import 'package:gooddhan/core/infrastructure/sembast_database.dart';
 final sembastProvider = Provider((ref) => SembastDatabase());
 
 final dioProvider = Provider((ref) => Dio());
+final connectivityProvider = Provider<Connectivity>((ref) {
+  return Connectivity();
+});
