ffbdcec0401434dd7d464ba3df143fe477b42856
sameer <i.amsameer2061@gmail.com>
2022-01-05 17:46:57 +0545

    success type

--- lib/dashboard/gooddhan/cateogries/core/application/paginated_category_notifier.dart
+++ lib/dashboard/gooddhan/cateogries/core/application/paginated_category_notifier.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:gooddhan/core/domain/fresh.dart';
-import 'package:gooddhan/dashboard/gooddhan/core/domain/PaginatedState.dart';
+import 'package:gooddhan/dashboard/gooddhan/core/domain/paginated_state.dart';
 import 'package:gooddhan/dashboard/gooddhan/core/domain/category.dart';
 import 'package:gooddhan/dashboard/gooddhan/core/domain/gooddhan_failure.dart';
 import 'package:gooddhan/dashboard/gooddhan/core/infrastructure/pagination_config.dart';
@@ -10,31 +10,32 @@ import 'package:gooddhan/dashboard/gooddhan/core/infrastructure/pagination_confi
 typedef CategoryGetter = Future<Either<GooddhanFailure, Fresh<List<Category>>>>
     Function(int page);
 
-class PaginatedCategoryNotifier extends StateNotifier<PaginatedCategoryState> {
-  PaginatedCategoryNotifier()
-      : super(PaginatedCategoryState.initial(Fresh.yes([])));
+class PaginatedCategoryNotifier
+    extends StateNotifier<PaginatedState<Fresh<List<Category>>>> {
+  PaginatedCategoryNotifier() : super(PaginatedState.initial(Fresh.yes([])));
 
   int _page = 1;
 
   @protected
   void resetPage() {
     _page = 1;
-    state = PaginatedCategoryState.initial(Fresh.yes([]));
+    state = PaginatedState.initial(Fresh.yes([]));
   }
 
   void getNextPage(CategoryGetter getter) async {
-    state = PaginatedCategoryState.loadInProgress(
-      state.categories,
+    state = PaginatedState.loadInProgress(
+      state.items,
       PaginationConfig.itermsPerPage,
     );
     final failureOrCategory = await getter(_page);
     state = failureOrCategory.fold(
-      (l) => PaginatedCategoryState.failed(state.categories, l),
+      (l) => PaginatedState.failed(state.items, l),
       (r) {
         _page++;
-        return PaginatedCategoryState.success(
-          r.copyWith(entity: [...state.categories.entity, ...r.entity]),
+        return PaginatedState.success(
+          r.copyWith(entity: [...state.items.entity, ...r.entity]),
           isNextPageAvilabel: r.isNextPageAvailable ?? false,
+          successType: SuccessType.fetched,
         );
       },
     );
--- lib/dashboard/gooddhan/cateogries/list_categories/application/list_categories_notifier.dart
+++ lib/dashboard/gooddhan/cateogries/list_categories/application/list_categories_notifier.dart
@@ -1,7 +1,7 @@
 import 'package:gooddhan/core/domain/fresh.dart';
 import 'package:gooddhan/dashboard/gooddhan/cateogries/core/application/paginated_category_notifier.dart';
 import 'package:gooddhan/dashboard/gooddhan/cateogries/list_categories/infrastrucuture/list_categories_repository.dart';
-import 'package:gooddhan/dashboard/gooddhan/core/domain/PaginatedState.dart';
+import 'package:gooddhan/dashboard/gooddhan/core/domain/paginated_state.dart';
 
 class ListCategoryNotifer extends PaginatedCategoryNotifier {
   final ListCategoriesRepository _repository;
@@ -18,31 +18,31 @@ class ListCategoryNotifer extends PaginatedCategoryNotifier {
   }
 
   Future<void> createCategory(String categoryName) async {
-    state = PaginatedCategoryState.loadInProgress(state.categories, 1);
+    state = PaginatedState.loadInProgress(state.items, 1);
     final failureOrCategory = await _repository.createCategory(categoryName);
     state = failureOrCategory.fold(
-      (l) => PaginatedCategoryState.failed(state.categories, l),
-      (r) => PaginatedCategoryState.success(
-        Fresh.yes([...state.categories.entity, r.toDomain()]),
-        isNextPageAvilabel: false,
-      ),
+      (l) => PaginatedState.failed(state.items, l),
+      (r) => PaginatedState.success(
+          Fresh.yes([...state.items.entity, r.toDomain()]),
+          isNextPageAvilabel: false,
+          successType: SuccessType.created),
     );
   }
 
   Future<void> deleteCategory(String id) async {
-    state = PaginatedCategoryState.loadInProgress(state.categories, 0);
+    state = PaginatedState.loadInProgress(state.items, 0);
     final failureOrCategory = await _repository.deleteCategory(id);
     state = failureOrCategory.fold(
-      (l) => PaginatedCategoryState.failed(state.categories, l),
-      (r) => PaginatedCategoryState.success(
-        Fresh.yes(
-          state.categories.entity
-            ..removeWhere(
-              (element) => element.id == id,
-            ),
-        ),
-        isNextPageAvilabel: false,
-      ),
+      (l) => PaginatedState.failed(state.items, l),
+      (r) => PaginatedState.success(
+          Fresh.yes(
+            state.items.entity
+              ..removeWhere(
+                (element) => element.id == id,
+              ),
+          ),
+          isNextPageAvilabel: false,
+          successType: SuccessType.deleted),
     );
   }
 }
