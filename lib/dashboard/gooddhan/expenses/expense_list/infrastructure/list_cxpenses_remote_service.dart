b50551fecbff81b666261daaafd1d321ad8c66df
sameer <i.amsameer2061@gmail.com>
2022-01-05 17:49:22 +0545

    list expense remote service

---
+++ lib/dashboard/gooddhan/expenses/expense_list/infrastructure/list_cxpenses_remote_service.dart
@@ -0,0 +1,44 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:dio/dio.dart';
+import 'package:gooddhan/core/infrastructure/remote_response.dart';
+import 'package:gooddhan/core/shared/constant.dart';
+import 'package:gooddhan/dashboard/gooddhan/core/infrastructure/expense_dto.dart';
+import 'package:gooddhan/dashboard/gooddhan/core/infrastructure/gooddhan_header_cache.dart';
+import 'package:gooddhan/dashboard/gooddhan/core/infrastructure/pagination_config.dart';
+import 'package:gooddhan/dashboard/gooddhan/expenses/core/infrastructure/expenses_remote_service.dart';
+
+class ListExpenseRemoteService extends ExpensesRemoteService {
+  ListExpenseRemoteService(
+    Dio dio,
+    Connectivity connectivity,
+    GooddhanHeaderCache headerCache,
+  ) : super(dio, connectivity, headerCache);
+
+  Future<RemoteResponse<List<ExpenseDTO>>> getExpensesListPage(int page) {
+    return super.getPage(
+      requestUri: Uri.parse(
+        "$baseUrl/expenses?page=$page&limit=${PaginationConfig.itermsPerPage}&sort=createdAt",
+      ),
+      jsonDataSelector: (data) => data["data"] as List<dynamic>,
+    );
+  }
+
+  Future<ExpenseDTO> createNewExpense(
+    String categoryId,
+    num amount,
+    String title,
+  ) {
+    return super.createExpense(
+      amount: amount,
+      categoryId: categoryId,
+      title: title,
+      requestUri: Uri.parse("$baseUrl/expenses"),
+    );
+  }
+
+  Future<void> deleteSingleExpense(String id) {
+    return super.deleteExpense(
+      requestUri: Uri.parse("$baseUrl/expenses/$id"),
+    );
+  }
+}
