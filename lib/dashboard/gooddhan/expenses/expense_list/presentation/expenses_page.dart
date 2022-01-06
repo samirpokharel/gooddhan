7d2dca5f8219f9ef82f0ba538ea7f50c014bfd20
sameer <i.amsameer2061@gmail.com>
2022-01-06 14:29:59 +0545

    filter bar on expense page

--- lib/dashboard/gooddhan/expenses/expense_list/presentation/expenses_page.dart
+++ lib/dashboard/gooddhan/expenses/expense_list/presentation/expenses_page.dart
@@ -4,13 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gooddhan/core/presentation/routes/app_router.gr.dart';
 import 'package:gooddhan/core/shared/clear_foucs.dart';
-import 'package:gooddhan/core/shared/toasts.dart';
 import 'package:gooddhan/core/shared/widgets/custom_loading_wrapper.dart';
-import 'package:gooddhan/dashboard/gooddhan/cateogries/core/presentation/paginated_category_list_view.dart';
-import 'package:gooddhan/dashboard/gooddhan/cateogries/core/shared/providers.dart';
-import 'package:gooddhan/dashboard/gooddhan/core/domain/paginated_state.dart';
 import 'package:gooddhan/dashboard/gooddhan/expenses/core/presentation/paginated_expense_list_view.dart';
 import 'package:gooddhan/dashboard/gooddhan/expenses/core/shared/providers.dart';
+import 'package:gooddhan/filter/domain/filter.dart';
+import 'package:gooddhan/filter/presentation/filter_bar.dart';
 
 const String noResultMessage =
     "That's about everything we could find in your starred repos right now.";
@@ -40,55 +38,73 @@ class _CategoriesPageState extends ConsumerState<ExpensesPage> {
         notifier.getFirstExpenseListPage();
       },
       child: ClearFocus(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text("Expenses"),
-            bottom: PreferredSize(
+        child: SafeArea(
+          child: Scaffold(
+            appBar: PreferredSize(
               child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: TextFormField(
-                  textInputAction: TextInputAction.search,
-                  onChanged: (val) {},
-                  decoration: const InputDecoration(
-                    prefixIcon: Icon(CupertinoIcons.search),
-                    hintText: "Search...",
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(CupertinoIcons.search),
+                          hintText: "Search Expenses..",
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    FilterBar(
+                      onApplyFilter: (Filter filter) {
+                        notifier.getFirstExpenseListPage(
+                          filter: filter,
+                        );
+                      },
+                      onFetchingDate: (Filter filter) {
+                        notifier.getFirstExpenseListPage(
+                          filter: filter,
+                        );
+                      },
+                    )
+                  ],
+                ),
+              ),
+              preferredSize: const Size.fromHeight(120),
+            ),
+            body: CustomLoadingWraper(
+              isLoading: ref.watch(listExpensesNotifierProvider).when(
+                    initial: (_) => false,
+                    loadInProgress: (_, __) => true,
+                    success: (_, __, ___) => false,
+                    failed: (_, __) => false,
+                  ),
+              child: Scaffold(
+                primary: true,
+                body: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: PaginatedExpensesListView(
+                    paginatedExpenseNotifierProvider:
+                        listExpensesNotifierProvider,
+                    getNextPage: () => notifier.getNextExpenseListPage(),
+                    onRefresh: () => notifier.getFirstExpenseListPage(),
+                    noResultMessage: noResultMessage,
                   ),
                 ),
               ),
-              preferredSize: const Size.fromHeight(60),
             ),
-          ),
-          body: CustomLoadingWraper(
-            isLoading: ref.watch(listExpensesNotifierProvider).when(
-                  initial: (_) => false,
-                  loadInProgress: (_, __) => true,
-                  success: (_, __, ___) => false,
-                  failed: (_, __) => false,
-                ),
-            child: Scaffold(
-              primary: true,
-              body: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: PaginatedExpensesListView(
-                  paginatedExpenseNotifierProvider:
-                      listExpensesNotifierProvider,
-                  getNextPage: () => notifier.getNextExpenseListPage(),
-                  onRefresh: () => notifier.getFirstExpenseListPage(),
-                  noResultMessage: noResultMessage,
-                ),
-              ),
+            floatingActionButton: FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () {
+                AutoRouter.of(context).push(
+                  CreateExpenseRoute(
+                    isUpdate: false,
+                    previousExpense: null,
+                  ),
+                );
+              },
             ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              AutoRouter.of(context).push(
-                CreateExpenseRoute(
-                  isUpdate: false,
-                  previousExpense: null,
-                ),
-              );
-            },
           ),
         ),
       ),
