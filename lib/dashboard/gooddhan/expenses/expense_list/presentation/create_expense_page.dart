90b1a06b0608cf7e8c763216df8ce747f70e906e
sameer <i.amsameer2061@gmail.com>
2022-01-09 18:33:50 +0545

    create expense improvement and fixes

--- lib/dashboard/gooddhan/expenses/expense_list/presentation/create_expense_page.dart
+++ lib/dashboard/gooddhan/expenses/expense_list/presentation/create_expense_page.dart
@@ -1,13 +1,17 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gooddhan/core/infrastructure/text_theme_extension.dart';
 import 'package:gooddhan/core/presentation/themes/app_icons.dart';
+import 'package:gooddhan/core/shared/toasts.dart';
+import 'package:gooddhan/core/shared/widgets/custom_loading_wrapper.dart';
 import 'package:gooddhan/core/shared/widgets/custom_state_button.dart';
 import 'package:gooddhan/core/shared/widgets/custom_value_tile.dart';
 import 'package:gooddhan/dashboard/gooddhan/cateogries/core/shared/providers.dart';
 import 'package:gooddhan/dashboard/gooddhan/cateogries/list_categories/presentation/category_picker.dart';
 import 'package:gooddhan/dashboard/gooddhan/core/domain/category.dart';
 import 'package:gooddhan/dashboard/gooddhan/core/domain/expense.dart';
+import 'package:gooddhan/dashboard/gooddhan/core/domain/paginated_state.dart';
 import 'package:gooddhan/dashboard/gooddhan/expenses/core/shared/providers.dart';
 
 class CreateExpensePage extends ConsumerStatefulWidget {
@@ -70,7 +74,6 @@ class _CreateExpensePageState extends ConsumerState<CreateExpensePage> {
         _selectedCategories.isNotEmpty &&
         _summaryController.text.isNotEmpty) {
       if (widget.isUpdate && widget.previousExpense != null) {
-        Navigator.pop(context);
         await notifier.updateExpense(
           categoryId: _selectedCategories[0].id,
           amount: num.parse(_expenseAmountController.text.trim()),
@@ -78,7 +81,6 @@ class _CreateExpensePageState extends ConsumerState<CreateExpensePage> {
           expenseId: widget.previousExpense!.id,
         );
       } else {
-        Navigator.pop(context);
         await notifier.createExpense(
           _selectedCategories[0].id,
           num.parse(_expenseAmountController.text),
@@ -99,96 +101,120 @@ class _CreateExpensePageState extends ConsumerState<CreateExpensePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16 * 2),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(
-                      AppIcon.arrowLeft,
-                      size: 20,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  const SizedBox(height: 32),
-                  Text(
-                    widget.isUpdate ? "Update Expense" : "Add Expenses",
-                    style: context.headline1,
-                  ),
-                  const SizedBox(height: 16),
-                  Text(
-                    widget.isUpdate
-                        ? "Update your expence"
-                        : "Add your expence",
-                    style: context.bodyText2,
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 16),
-                  const Divider(),
-                  CustomValueTile(
-                    onTap: () => selectCategory(),
-                    title: "Choose Category",
-                    value: "${_selectedCategories.length} selected",
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    controller: _summaryController,
-                    decoration: const InputDecoration(hintText: "Summary"),
-                  ),
-                  const SizedBox(height: 32),
-                  const Text("How much ?"),
-                  const SizedBox(height: 16),
-                  Theme(
-                    data: ThemeData(),
-                    child: Row(
-                      children: [
-                        const Text(
-                          "Rs",
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w900,
-                          ),
+    ref.listen<PaginatedState>(listExpensesNotifierProvider, (_, next) {
+      next.maybeWhen(
+        failed: (_, failure) {
+          showFlashToast(
+            context,
+            message: failure.message,
+            flavouer: ToastFlavouer.error,
+            dismissDuration: const Duration(seconds: 3),
+          );
+        },
+        success: (_, __, ___) {
+          AutoRouter.of(context).pop();
+        },
+        orElse: () {},
+      );
+    });
+    return SafeArea(
+      child: CustomLoadingWraper(
+        isLoading: ref.watch(listExpensesNotifierProvider).maybeWhen(
+              orElse: () => false,
+              loadInProgress: (_, __) => true,
+            ),
+        child: Scaffold(
+          body: Form(
+            key: _formKey,
+            child: SafeArea(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16 * 2),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(
+                          AppIcon.arrowLeft,
+                          size: 20,
+                          color: Theme.of(context).primaryColor,
                         ),
-                        const SizedBox(width: 20),
-                        Expanded(
-                          child: TextFormField(
-                            controller: _expenseAmountController,
-                            style: const TextStyle(
-                              fontSize: 40,
-                              fontWeight: FontWeight.w900,
-                            ),
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              hintText: "0.00",
-                              border: InputBorder.none,
-                              hintStyle: TextStyle(
-                                color: Color(0xffA5A5A5),
+                      ),
+                      const SizedBox(height: 32),
+                      Text(
+                        widget.isUpdate ? "Update Expense" : "Add Expenses",
+                        style: context.headline1,
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        widget.isUpdate
+                            ? "Update your expence"
+                            : "Add your expence",
+                        style: context.bodyText2,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 16),
+                      const Divider(),
+                      CustomValueTile(
+                        onTap: () => selectCategory(),
+                        title: "Choose Category",
+                        value: "${_selectedCategories.length} selected",
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: _summaryController,
+                        decoration: const InputDecoration(hintText: "Summary"),
+                      ),
+                      const SizedBox(height: 32),
+                      const Text("How much ?"),
+                      const SizedBox(height: 16),
+                      Theme(
+                        data: ThemeData(),
+                        child: Row(
+                          children: [
+                            const Text(
+                              "Rs",
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.w900,
                               ),
                             ),
-                          ),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              child: TextFormField(
+                                controller: _expenseAmountController,
+                                style: const TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                                keyboardType: TextInputType.number,
+                                decoration: const InputDecoration(
+                                  hintText: "0.00",
+                                  border: InputBorder.none,
+                                  hintStyle: TextStyle(
+                                    color: Color(0xffA5A5A5),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 32),
+                      Align(
+                        alignment: Alignment.center,
+                        child: CustomStateButton(
+                          buttonStatus: ButtonStatus.idle,
+                          text: "Continue",
+                          onPressed: createOrUpdate,
+                        ),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 32),
-                  Align(
-                    alignment: Alignment.center,
-                    child: CustomStateButton(
-                      buttonStatus: ButtonStatus.idle,
-                      text: "Continue",
-                      onPressed: createOrUpdate,
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ),
